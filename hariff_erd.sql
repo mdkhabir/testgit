5ff6b5da6c9463556bcecb37c5570e724f661b1b
Md. Khabir uddin khan <mdkhabir@gmail.com>
2013-02-21 21:43:19 +0600

    Update hariff_erd.sql

--- hariff_erd.sql
+++ hariff_erd.sql
@@ -5,16 +5,7 @@
 -- Host: localhost
 -- Generation Time: Feb 17, 2013 at 02:16 AM
 -- Server version: 5.5.23
--- PHP Version: 5.2.17
 
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
 
 --
 -- Database: `hariff_erd`
